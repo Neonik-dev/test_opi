d33cdae3383fe265b81d9c655ca002905f6b8fed
belovlaska <forolimpvk@gmail.com>
2024-04-02 21:37:00 +0300

    Revert "Revert "r0""
    This reverts commit 4b59ef782aac7135e348cc3a8b6cb494a6e20cf0.

---
+++ commit0/D.java
@@ -0,0 +1,14 @@
+public class D {
+
+    private long g = 4321;
+
+    private long d = 1234;
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public void ab() {
+        System.out.println();
+    }
+}
