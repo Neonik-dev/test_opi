45723901c50c882174c2daaf8d7d66a5d44ebbce
belovlaska <forolimpvk@gmail.com>
2024-04-02 21:35:45 +0300

    Revert "Revert "r1""
    This reverts commit 708b853acd3a31a838e1a0170e43a2da724d4cc1.

---
+++ commit1/E.java
@@ -0,0 +1,18 @@
+public class E {
+
+    private double a = 100.500;
+
+    private double i = 100.500;
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+}
