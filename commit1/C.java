bf302b650945d4570c905f68b4f93eaf7f3063cd
belovlaska <forolimpvk@gmail.com>
2024-04-02 21:09:30 +0300

    r1

---
+++ commit1/C.java
@@ -0,0 +1,18 @@
+public class C extends E {
+
+    private int g = 1;
+
+    private double h = 100.500;
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public double ee() {
+        return java.lang.Math.PI;
+    }
+
+    public Object pp() {
+        return this;
+    }
+}
---
+++ commit1/D.java
@@ -0,0 +1,18 @@
+public class D {
+
+    private long g = 4321;
+
+    private long d = 1234;
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public void ab() {
+        System.out.println();
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+}
